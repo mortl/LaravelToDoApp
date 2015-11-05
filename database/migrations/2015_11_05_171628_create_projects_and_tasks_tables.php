9c499cac9413f4fa9d96293b157cf7dffa501262
mortl <martin.benes66@gmail.com>
2015-11-05 16:49:26 -0500

    Second commit

---
+++ database/migrations/2015_11_05_171628_create_projects_and_tasks_tables.php
@@ -0,0 +1,39 @@
+<?php                                                                                                
+    use Illuminate\Database\Schema\Blueprint;                                                        
+	use Illuminate\Database\Migrations\Migration;                                                    
+                                                                                                     
+	class CreateProjectsAndTasksTables extends Migration{                                            
+		                                                                                             
+		                                                                                             
+		public function up(){                                                                        
+			                                                                                         
+			Schema::create('projects',function(Blueprint $table)                                     
+			{                                                                                        
+				$table->increments('id');                                                            
+				$table->string('name')->default('');                                                 
+				$table->string('slug')->default('');                                                 
+				$table->timestamps();                                                                
+				                                                                                     
+			});                                                                                      
+			                                                                                         
+			Schema::create('tasks',function(Blueprint $table)                                        
+			{                                                                                        
+				$table->increments('id');                                                            
+				$table->integer('project_id')->unsigned()->default(0);                               
+				$table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+				$table->string('name')->default('');                                                 
+				$table->string('slug')->default('');                                                 
+				$table->boolean('completed')->default(false);                                        
+				$table->text('description')->default('');                                            
+				$table->timestamps();                                                                
+			});                                                                                      
+		}                                                                                            
+		                                                                                             
+		public function down(){                                                                      
+			                                                                                         
+			Schema::drop('tasks');                                                                   
+			Schema::drop('projects');                                                                
+			                                                                                         
+		}                                                                                            
+	}                                                                                                
+                                                                                                     
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+ 
+@section('content')
+    <h2>{{ $project->name }}</h2>
+@endsection
